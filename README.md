999f4ac32f50156dc165ba7d211e29b2b31c47d7
magasp <marie.gasparoux@u-bordeaux.fr>
2015-11-20 14:08:15 +0100

    README + fixing create_db.sh

--- README.md
+++ README.md
@@ -1,11 +1,18 @@
 # Metaviro-II
-Repository of the Metaviro-II Machine Learning project
+Repository of the *Metaviro-II Machine Learning* project
+
+## Prerequisite software
+
+- [Vowpal Wabbit](https://github.com/JohnLangford/vowpal_wabbit/) machine learning system (package `vowpal-wabbit` available for some Debian-based distributions)
+- Python 3 with module [Biopython](http://biopython.org/wiki/Main_Page) (package `python3-biopython` or `python-biopython` available for some distributions)
+- SQLite3
 
 ## Installation
 
 ```
 $ sh/create_db.sh
 ```
+Will take some time. At least 25G of space needed.
 
 ## Begin classification
 
--- sh/create_db.sh
+++ sh/create_db.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-path='sqlite/'
+path='taxdump/'
 pypath='../python/'
 db='ncbi.db'
 
 cd $path
-# echo 'Downloading taxonomy files...'
-# wget ftp://ftp.ncbi.nih.gov/pub/taxonomy/gi_taxid_nucl.dmp.gz
-# gzip -d gi_taxid_nucl.dmp.gz
-# wget ftp://ftp.ncbi.nih.gov/pub/taxonomy/taxdump.tar.Z
-# tar -xvzf taxdump.tar.Z
+echo 'Downloading taxonomy files...'
+wget ftp://ftp.ncbi.nih.gov/pub/taxonomy/gi_taxid_nucl.dmp.gz
+gzip -d gi_taxid_nucl.dmp.gz
+wget ftp://ftp.ncbi.nih.gov/pub/taxonomy/taxdump.tar.Z
+tar -xvzf taxdump.tar.Z
 
 echo 'Creating taxonomy database...'
 sqlite3 $db < create_db.sql
@@ -19,7 +19,7 @@ sqlite3 $db < speed_db.sql
 echo 'Testing database'
 sqlite3 $db < test_db.sql
 
-# rm taxdump.tar.Z
-# rm *.dmp
-# rm *.txt
-# rm *.prt
+rm taxdump.tar.Z
+rm *.dmp
+rm *.txt
+rm *.prt
